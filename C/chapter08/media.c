c03755e6fab24356fc41a55de449d933e10507e4
ILoveFryingPan <2669206166@qq.com>
2019-10-14 20:43:32 +0800

    第八章节第一次提交

---
+++ C/chapter08/media.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAX_LENGTH 40
+#define MAX_FILE_LENGTH 255
+#define GET_NAME "%255s"
+
+int ctoi(char *c) {
+	int result = 0;
+	int temp = 0;
+	while(*c) {
+		temp = *c - '0';
+		if(temp >= 0 && temp <= 9) {
+			result = result * 10 + temp;
+		}
+		c++;
+	}
+	return result;
+}
+
+int main() {
+	int i, j, l = 0;
+	char f_list[MAX_LENGTH][MAX_FILE_LENGTH + 1];
+	char *p_list[MAX_LENGTH];
+	for(i = 0; i < MAX_LENGTH; i++) {
+		p_list[i] = NULL;
+		f_list[i][0] = 0;
+	}
+
+	int select_value;
+	char exit_switch = 1;
+	char *temp = malloc(sizeof(char) * 10);
+	do{
+		puts("请选择操作命令：");
+		puts("（1）添加新文件：");
+		puts("（2）删除列表中的文件");
+		puts("（3）打印播放列表");
+		puts("（4）推出程序");
+		puts("（5）按文件名排序");
+
+		scanf("%s\n", temp);
+		if(strlen(temp) != 1) {
+			continue;
+		}
+
+		select_value = ctoi(temp);
+		printf("输入的数字是：%d\n", select_value);
+		switch(select_value) {
+			case 1:
+				if(l < MAX_LENGTH) {
+					for(i = 0; i < MAX_LENGTH; i++) {
+						if(f_list[i][0] == 0) {
+							printf("输入文件名：");
+							scanf(GET_NAME, f_list[i]);
+							p_list[i] = f_list[i];
+							l++;
+							break;
+						}
+					}
+				} else {
+					printf("列表满了\n");
+				}
+				break;
+			case 2:
+				if(l > 0) {
+					printf("输入文件序号：\n");
+					scanf("%s", temp);
+					int index = ctoi(temp);
+					if(index > 0 && index <= l) {
+						for(i = index - 1; i < l - 1;i++) {
+							p_list[i] = p_list[i + 1];
+						}
+						f_list[--index][0] = 0;
+					}
+				} else {
+					printf("列表没数据");
+				}
+				break;
+			case 3:
+				for(i = 0; i < l; i++) {
+					printf("%d : %s", (i + 1), p_list[i]);
+				}
+				break;
+			case 4:
+				exit_switch = 0;
+				break;
+			case 5:
+				break;
+			default:
+				printf("别瞎输入\n");
+				break;
+		}
+		} while(exit_switch);
+
+	return 0;
+}
