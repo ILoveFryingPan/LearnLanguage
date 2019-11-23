c0874c6f8818f866df221e4a33bfc43572f8c317
ILoveFryingPan <2669206166@qq.com>
2019-11-23 15:17:01 +0800

    整理文件

--- C/exer/main_arg.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(int argc, char *argv[])
-{
-	printf("参数个数：%d\n", argc);
-	for(int i = 0; i < argc; i++) {
-		printf("参数%d的值：%s\n", i, argv[i]);
-	}
-	return 0;
-}
--- C/exercise/ws.c
+++ C/exercise/ws.c
@@ -3,17 +3,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void input(char *format, void *address[])
+{
+	int format_num;
+	int address_num = sizeof(address) / sizeof(*);
+
+	scanf(format, address);
+	scanf("%*[^\n]%*c");
+}
+
 int main(int argc, char *argv[])
 {
 	char *s = (char *) malloc(3);
 	char getBuff;
 	printf("输入一段字符串\n");
-	while(scanf("%10s", s)) {
-		scanf("%*[^\n]%*c");
-		
-		//while(getBuff = getchar() != '\n' && getBuff != EOF);
-		printf("输入的字符串是：%s\n", s);
-	}
+	//while(scanf("%10s", s)) {
+	//	scanf("%*[^\n]%*c");
+	//	
+	//	//while(getBuff = getchar() != '\n' && getBuff != EOF);
+	//	printf("输入的字符串是：%s\n", s);
+	//}
 	printf("输入结束\n");
+	free(s);
+
+	int m;
+	char *p = (char *) malloc(11);
+	char n;
+	void *address[] = {&m, p, &n};
+	input("%d", &m);
+	printf("%d", m);
 	return 0;
 }
